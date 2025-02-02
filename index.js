5498110ef441f61543bd6608caf6b58c2d2d3ae1
Yuragranovsky <yuragranovsky@gmail.com>
2025-02-02 21:05:30 +0200

    number Math functions

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+// let a=2;
+// a="abc";
+// console.log(a);
+// const b = 3;
+// b = "lmn"; - const cannot be updated
+//primitive types
+/* 
+1. number like double in Java
+2. string "abc" < "b"
+3. boolean (true, false)
+4. undefined (undefined)
+5. null (null)
+*/
+//1. number
+let a = 100;
+a /= 3;
+console.log("non-normalized result of division 100 on 3",a)
+a = Math.trunc(a);
+console.log("normalized result of division 100 on 3",a);
+a = Math.round(30.6);
+console.log("30.6 rounded - ", a)
+console.log("30.1 after method floor - ", Math.floor(30.1));
+console.log("30.1 after method ceil - ", Math.ceil(30.1));
