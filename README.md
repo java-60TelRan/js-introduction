8d4ba8ecb02a05e455f258eb140a6c18b4ccee28
Yuragranovsky <yuragranovsky@gmail.com>
2025-02-12 21:09:12 +0200

    HW #13 definition

---
+++ README.md
@@ -0,0 +1,4 @@
+# HW# 13 definition
+## write the method "some" (see comments)
+## write the method "every" (see comments)
+## write more test cases (console.log functionality)
--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Hello World");
+let array = ["abc", 23];
+//array[1000000] = 10;
+// console.log(array.length);
+// console.log(array[0])}
+function forEach (array, fun) {
+     for(let i = 0; i < array.length; i++) {
+        fun(array[i], i);
+     }
+}
+//forEach takes array and fun with two parameters: first - element of array, second - index
+function print(elem, index){
+    console.log(`index: ${index}, element: ${elem}`)
+};
+forEach(array, print);
+function some(array, fun) {
+    //TODO
+    //returns true if at least one element of the array match a condition given in the function (fun)
+}
+function evenNumber(num) {
+    return num % 2 == 0;
+}
+console.log(`using "some" function for even numbers array: ${array}, function ${evenNumber}, result: ${some(array, evenNumber)} - false`)
+function every(array, fun) {
+//TODO
+    //returns true if all elemnts of the array match a condition given in the function (fun)
+}
+array = [2, 3, 4];
+function elmGreaterIndex(elem, index) {
+    return elem > index
+}
+console.log(`using "every" function for elements greater than the index values, array: ${array}, function ${elmGreaterIndex}, result: ${every(array, evenNumber)} - true`)
