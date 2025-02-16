5a8795734110e77293c96ede20689dc2d826f0d5
Yuragranovsky <yuragranovsky@gmail.com>
2025-02-16 18:51:21 +0200

    HW #13 solution (see README)

--- index.js
+++ index.js
@@ -1,31 +1,51 @@
-let array = ["abc", 23];
-//array[1000000] = 10;
-// console.log(array.length);
-// console.log(array[0])}
-function forEach (array, fun) {
-     for(let i = 0; i < array.length; i++) {
-        fun(array[i], i);
-     }
-}
-//forEach takes array and fun with two parameters: first - element of array, second - index
-function print(elem, index){
-    console.log(`index: ${index}, element: ${elem}`)
-};
-forEach(array, print);
+
 function some(array, fun) {
-    //TODO
-    //returns true if at least one element of the array match a condition given in the function (fun)
+    //returns true if at least one element of the array mathes condition of fun
+    let index = 0;
+   while(index < array.length && !fun(array[index], index) ) {
+    index++;
+   }
+   return index < array.length;
+}
+
+
+function every(array, fun) {
+    //returns true if all elements of the array match condition of fun
+    let index = 0;
+   while(index < array.length && fun(array[index], index) ) {
+    index++;
+   }
+   return index == array.length;
+}
+
+function elmGreaterIndex(elem, index) {
+    return elem > index
+}
+function elmLessIndex(elem, index) {
+    return elem < index
 }
 function evenNumber(num) {
     return num % 2 == 0;
 }
-console.log(`using "some" function for even numbers array: ${array}, function ${evenNumber}, result: ${some(array, evenNumber)} - false`)
-function every(array, fun) {
-//TODO
-    //returns true if all elemnts of the array match a condition given in the function (fun)
+function oddNumber(num) {
+    return num % 2 != 0;
 }
-array = [2, 3, 4];
-function elmGreaterIndex(elem, index) {
-    return elem > index
+function negativeNumber(num) {
+    return num < 0;
 }
-console.log(`using "every" function for elements greater than the index values, array: ${array}, function ${elmGreaterIndex}, result: ${every(array, evenNumber)} - true`)
+function positiveNumber(num) {
+    return num >0 ;
+}
+//test logs
+const arOnlyEven = [2, 4, 6, -8];
+const arOnlyOdd = [1, 3, 7, 5];
+const arOnlyNegativeValues = [-1, -2, -3];
+const arOnlyPositiveNumbers= [1 ,2, 3, 4];
+console.log(`using "every" , function for numbers greater than the index values, array: ${arOnlyPositiveNumbers}, function ${elmGreaterIndex}, result: ${every(arOnlyPositiveNumbers, elmGreaterIndex)} == true`);
+console.log(`using "some" function for even numbers array: ${arOnlyOdd}, function ${evenNumber}, result: ${some(arOnlyOdd, evenNumber)} == false`);
+console.log(`using "every" , function for numbers greater than the index values, array: ${arOnlyEven}, function ${elmGreaterIndex}, result: ${every(arOnlyEven, elmGreaterIndex)} == false`);
+console.log(`using "some" function for positive numbers array: ${arOnlyNegativeValues}, function ${positiveNumber}, result: ${some(arOnlyNegativeValues, positiveNumber)} == false`);
+console.log(`using "every" , function for numbers less than the index values, array: ${arOnlyNegativeValues}, function ${elmLessIndex}, result: ${every(arOnlyNegativeValues, elmLessIndex)} == true`);
+console.log(`using "some" function for numbers less than the index values, array: ${arOnlyEven}, function ${elmLessIndex}, result: ${some(arOnlyEven, elmLessIndex)} == true`);
+console.log(`using "every" , function for negative numbers, array: ${arOnlyNegativeValues}, function ${negativeNumber}, result: ${every(arOnlyNegativeValues, negativeNumber)} == true`);
+console.log(`using "some" function for positive numbers, array: ${arOnlyNegativeValues}, function ${positiveNumber}, result: ${some(arOnlyNegativeValues, positiveNumber)} == false`);
