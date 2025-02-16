e4ba538f7803aca4ff52b4bd7e55c7ab80cab7e8
Yuragranovsky <yuragranovsky@gmail.com>
2025-02-16 21:03:31 +0200

    HW #14 definition

---
+++ README.md
@@ -0,0 +1,8 @@
+# HW#14 definition
+## write function minMax(...values)
+### values - parameter matching any number of arguments (an argument may be either array or number)
+### returns array with two numbers: first number - minimal value, second number - maximal value
+## examples
+### minMax(1, 2, 3) => [1, 3]
+### minMax(1, 2, 3, [100, 50]) => [1, 100]
+### minMax(1, 2, 3, [100, 50], [-2, 40, 200]) => [-2, 200]
--- index.js
+++ index.js
@@ -1,26 +1,4 @@
-function sum (...operands) {
-    let sum = 0;
-   
-    for(let i = 0; i < operands.length; i++) {
-        let operand = operands[i]
-        if (Array.isArray(operand)) {
-            operand = sumArray(operand);
-        }
-        sum += operand;
-    }
-    return sum;
+function minMax(...values) {
+    //TODO
+    //see README
 }
-function sumArray(array) {
-    let sum = 0
-    for(let i = 0; i < array.length; i++){
-        sum += array[i];
-    }
-    return sum;
-}
-//examples of function sum
-console.log(`sum(1,2,3,4)=${sum(1,2,3,4)}`);
-console.log(`sum([1, 2, 3, 4])=${sum([1, 2, 3, 4])}`);
-console.log(`sum([1, 2, 3, 4], 5, 6)=${sum([1, 2, 3, 4], 5, 6)}`);
-console.log(`Math.max(1, 2, 3) = ${Math.max(1, 2, 3)}`)
-console.log(`Math.max(...[1, 2, 3]) = ${Math.max(...[1, 2, 3])}`)
-
