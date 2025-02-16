589ef3b3cd77bb36a1e03f9617b37330c12f4e4c
Yuragranovsky <yuragranovsky@gmail.com>
2025-02-16 20:34:50 +0200

    variadic parameter / spread operator

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 function sum (...operands) {
     let sum = 0;
+   
     for(let i = 0; i < operands.length; i++) {
-        sum = sum + operands[i];
+        let operand = operands[i]
+        if (Array.isArray(operand)) {
+            operand = sumArray(operand);
+        }
+        sum += operand;
+    }
+    return sum;
+}
+function sumArray(array) {
+    let sum = 0
+    for(let i = 0; i < array.length; i++){
+        sum += array[i];
     }
     return sum;
 }
 //examples of function sum
-console.log(`sum(1,2, "Hello")=${sum(1,2, "Hello")}`);
-//console.log(`sum([1, 2, 3])=${sum([1, 2, 3, 4])}`);
+console.log(`sum(1,2,3,4)=${sum(1,2,3,4)}`);
+console.log(`sum([1, 2, 3, 4])=${sum([1, 2, 3, 4])}`);
+console.log(`sum([1, 2, 3, 4], 5, 6)=${sum([1, 2, 3, 4], 5, 6)}`);
+console.log(`Math.max(1, 2, 3) = ${Math.max(1, 2, 3)}`)
+console.log(`Math.max(...[1, 2, 3]) = ${Math.max(...[1, 2, 3])}`)
+
