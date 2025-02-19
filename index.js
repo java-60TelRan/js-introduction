f347c654a827b1b5d9ec809032b1228398130d6e
Yuragranovsky <yuragranovsky@gmail.com>
2025-02-19 20:43:37 +0200

    finding/map/forEach/sort

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 const array = ['HELLO', 122, -10, 'Java', 'JavaScript', 500, 'Nodejs'];
-//adding to array
-//push adds to end of an source array
-//unshiftn adds at beginning of an source array
-//splice inserts at middle of array
-// array.push(...[1, 2, 3]);
-// console.log(array[7])
-//console.log([1, 2, 3].push([4, 5, 6]))
- //const array1 = array.concat(3, 4, 5)
-// console.log(array1[3])
-//concat - method that doesn't update source array. It only returns new array as a result of concatination
-// const ar1 = [3,4,5].concat(array);
-// console.log(ar1)
-// console.log(array.unshift(3, 4, 5))
-// console.log(array);
-// 
-/***************************************************************** */
-//removing last element
-// console.log(array.pop())
-// console.log(array)
-//removing first element
-// console.log(array.shift());
-// console.log(array)
-//removing several elements or at middle
-// console.log(array.splice(4, 3));
-// console.log(array)
-console.log(array.splice(3, 1, "Python"))
-console.log(array)
+//finding
+// let index = array.indexOf("Java");
+// console.log(index);
+// const array1 = array.filter(function(e) {
+//     let res = true;
+//     if(typeof e == "string") {
+//         res = !e.includes("Java")
+//     }
+//     return res;
+// })
+// console.log(array1)
+//forEach
+// array.forEach(function(e, i) {
+//     console.log(`${i + 1}: ${e}`)
+// })
+//map
+// const array1 = array.map(function(e) {
+//     let res = typeof e == "number" ? Math.abs(e).toString().length: e.length;
+//     return res;
+
+// })
+// console.log(array1)
+//join
+// const str = array.join("<br>");
+// console.log(str);
+const array1 = [122, 10000, 2, 9, 100000000];
+// console.log(array1.sort());
+// console.log(array1)
+// console.log(array1.toSorted());
+// console.log(array1);
+console.log(array1.toSorted(function(a, b){
+    return b - a;
+}))
