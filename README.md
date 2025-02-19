e7441c0f0569ecb92c06f303b2c3c956639ba4af
Yuragranovsky <yuragranovsky@gmail.com>
2025-02-19 19:51:56 +0200

    add /remove /update

--- README.md
+++
@@ -1,4 +0,0 @@
-# HW# 13 definition
-## write the method "some" (see comments)
-## write the method "every" (see comments)
-## write more test cases (console.log functionality)
--- index.js
+++ index.js
@@ -1,51 +1,28 @@
-
-function some(array, fun) {
-    //returns true if at least one element of the array mathes condition of fun
-    let index = 0;
-   while(index < array.length && !fun(array[index], index) ) {
-    index++;
-   }
-   return index < array.length;
-}
-
-
-function every(array, fun) {
-    //returns true if all elements of the array match condition of fun
-    let index = 0;
-   while(index < array.length && fun(array[index], index) ) {
-    index++;
-   }
-   return index == array.length;
-}
-
-function elmGreaterIndex(elem, index) {
-    return elem > index
-}
-function elmLessIndex(elem, index) {
-    return elem < index
-}
-function evenNumber(num) {
-    return num % 2 == 0;
-}
-function oddNumber(num) {
-    return num % 2 != 0;
-}
-function negativeNumber(num) {
-    return num < 0;
-}
-function positiveNumber(num) {
-    return num >0 ;
-}
-//test logs
-const arOnlyEven = [2, 4, 6, -8];
-const arOnlyOdd = [1, 3, 7, 5];
-const arOnlyNegativeValues = [-1, -2, -3];
-const arOnlyPositiveNumbers= [1 ,2, 3, 4];
-console.log(`using "every" , function for numbers greater than the index values, array: ${arOnlyPositiveNumbers}, function ${elmGreaterIndex}, result: ${every(arOnlyPositiveNumbers, elmGreaterIndex)} == true`);
-console.log(`using "some" function for even numbers array: ${arOnlyOdd}, function ${evenNumber}, result: ${some(arOnlyOdd, evenNumber)} == false`);
-console.log(`using "every" , function for numbers greater than the index values, array: ${arOnlyEven}, function ${elmGreaterIndex}, result: ${every(arOnlyEven, elmGreaterIndex)} == false`);
-console.log(`using "some" function for positive numbers array: ${arOnlyNegativeValues}, function ${positiveNumber}, result: ${some(arOnlyNegativeValues, positiveNumber)} == false`);
-console.log(`using "every" , function for numbers less than the index values, array: ${arOnlyNegativeValues}, function ${elmLessIndex}, result: ${every(arOnlyNegativeValues, elmLessIndex)} == true`);
-console.log(`using "some" function for numbers less than the index values, array: ${arOnlyEven}, function ${elmLessIndex}, result: ${some(arOnlyEven, elmLessIndex)} == true`);
-console.log(`using "every" , function for negative numbers, array: ${arOnlyNegativeValues}, function ${negativeNumber}, result: ${every(arOnlyNegativeValues, negativeNumber)} == true`);
-console.log(`using "some" function for positive numbers, array: ${arOnlyNegativeValues}, function ${positiveNumber}, result: ${some(arOnlyNegativeValues, positiveNumber)} == false`);
+const array = ['HELLO', 122, -10, 'Java', 'JavaScript', 500, 'Nodejs'];
+//adding to array
+//push adds to end of an source array
+//unshiftn adds at beginning of an source array
+//splice inserts at middle of array
+// array.push(...[1, 2, 3]);
+// console.log(array[7])
+//console.log([1, 2, 3].push([4, 5, 6]))
+ //const array1 = array.concat(3, 4, 5)
+// console.log(array1[3])
+//concat - method that doesn't update source array. It only returns new array as a result of concatination
+// const ar1 = [3,4,5].concat(array);
+// console.log(ar1)
+// console.log(array.unshift(3, 4, 5))
+// console.log(array);
+// 
+/***************************************************************** */
+//removing last element
+// console.log(array.pop())
+// console.log(array)
+//removing first element
+// console.log(array.shift());
+// console.log(array)
+//removing several elements or at middle
+// console.log(array.splice(4, 3));
+// console.log(array)
+console.log(array.splice(3, 1, "Python"))
+console.log(array)
