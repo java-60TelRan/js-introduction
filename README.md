329540844d30fb25731bc81507c9cfd2853ac724
Yuragranovsky <yuragranovsky@gmail.com>
2025-02-19 21:12:09 +0200

    HW #15 definition

---
+++ README.md
@@ -0,0 +1,4 @@
+# HW#15 definition
+## Write two functions:
+### compNumStr (see comments in index.js)
+### orderedList (see comments in index.js)
--- index.js
+++ index.js
@@ -1,34 +1,29 @@
 const array = ['HELLO', 122, -10, 'Java', 'JavaScript', 500, 'Nodejs'];
-//finding
-// let index = array.indexOf("Java");
-// console.log(index);
-// const array1 = array.filter(function(e) {
-//     let res = true;
-//     if(typeof e == "string") {
-//         res = !e.includes("Java")
-//     }
-//     return res;
-// })
-// console.log(array1)
-//forEach
-// array.forEach(function(e, i) {
-//     console.log(`${i + 1}: ${e}`)
-// })
-//map
-// const array1 = array.map(function(e) {
-//     let res = typeof e == "number" ? Math.abs(e).toString().length: e.length;
-//     return res;
-
-// })
-// console.log(array1)
-//join
-// const str = array.join("<br>");
-// console.log(str);
-const array1 = [122, 10000, 2, 9, 100000000];
-// console.log(array1.sort());
-// console.log(array1)
-// console.log(array1.toSorted());
-// console.log(array1);
-console.log(array1.toSorted(function(a, b){
-    return b - a;
-}))
+function compNumStr(e1, e2) {
+    //TODO
+    //compares e1 and e2 based on:
+   //returns negative, positive or 0 so that
+    //sorting was as follows
+    //first numbers sorted by descending order
+    //after numbers strings sorted by ascending order
+    //example: array.sort(compNumStr);
+    //array will be  [500, 122, -10, 'HELLO', 'Java', 'JavaScript','Nodejs']
+}
+function orderedList(array) {
+    //TODO
+    //array is any array containing both numbewrs and strings
+    //returns HTML text for ordered list
+    //string will have default color
+    //number will have red color
+    //<ol><li>....</li>...<li>....</li></ol>
+    //example (only ewxample)
+    return `<ol>
+        <li class="item">Hello</li>
+        <li class="item item_number">300</li>
+        <li class="item">Java</li>
+        <li class="item">Nodejs</li>
+        <li class="item item_number">100</li>
+    </ol>`
+}
+const bodyElement = document.querySelector('body');
+bodyElement.innerHTML = orderedList(["Hello", 300, "Java", "Nodejs", 100]);
