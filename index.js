ecf12f3bf7342a0a0e35e9d90a055d0b6305006f
Yuragranovsky <yuragranovsky@gmail.com>
2025-02-23 18:34:06 +0200

    HW #15 solution

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const array = ['HELLO', 122, -10, 'Java', 'JavaScript', 500, 'Nodejs'];
+function isNumber(element) {
+  return typeof element == "number";
+}
 function compNumStr(e1, e2) {
-    //TODO
     //compares e1 and e2 based on:
    //returns negative, positive or 0 so that
     //sorting was as follows
@@ -8,22 +10,36 @@ function compNumStr(e1, e2) {
     //after numbers strings sorted by ascending order
     //example: array.sort(compNumStr);
     //array will be  [500, 122, -10, 'HELLO', 'Java', 'JavaScript','Nodejs']
+    let res = 1;
+    if(isNumber(e1) && isNumber(e2)) {
+      res = e2 - e1;
+    } else if (!isNumber(e1) && !isNumber(e2)) {
+      res = e1 > e2 ? 1 : -1
+    } else if (isNumber(e1) && !isNumber(e2)) {
+      res = -1;
+    }
+    return res;
 }
+array.sort(compNumStr);
 function orderedList(array) {
-    //TODO
     //array is any array containing both numbewrs and strings
     //returns HTML text for ordered list
     //string will have default color
     //number will have red color
     //<ol><li>....</li>...<li>....</li></ol>
     //example (only ewxample)
+    const items = getItems(array).join(" ");
     return `<ol>
-        <li class="item">Hello</li>
-        <li class="item item_number">300</li>
-        <li class="item">Java</li>
-        <li class="item">Nodejs</li>
-        <li class="item item_number">100</li>
+       ${items}
     </ol>`
 }
+function getItems(array) {
+  const res = array.map(getItem);
+  return res
+}
+function getItem(element) {
+  const res =  `<li class="item ${isNumber(element) ? "item_number" : ""}">${element}</li>`
+  return res;
+}
 const bodyElement = document.querySelector('body');
-bodyElement.innerHTML = orderedList(["Hello", 300, "Java", "Nodejs", 100]);
+bodyElement.innerHTML = orderedList(array);
