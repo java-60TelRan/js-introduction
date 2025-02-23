b553c1f32f76baf5f4782417c651996c4df2e0b6
Yuragranovsky <yuragranovsky@gmail.com>
2025-02-23 19:18:45 +0200

    arrow function introduction

--- index.js
+++ index.js
@@ -1,36 +1,30 @@
-function minMax(...values) {
-    //see README
-    const res = getInitialRes(values[0]); //getting array with first element as min value and second as max
-    for (let i = 0; i < values.length; i++) {
-            if(Array.isArray(values[i])) {
-                 minMaxArray(values[i], res);
-                
-            } else {
-                minMaxElement(values[i], res);
-            }
-    }
-    return res;
+//arrow functions
+const sumFun = function(op1, op2) {
+  return op1 + op2;
 }
-function getInitialRes(value){
-  return Array.isArray(value) ? [value[0], value[0]] : [value, value];
+const sumArrowFun = (op1, op2) => op1 + op2;
+//statement
+// let c, a = 10, b = 3;
+// if( a > b) {
+//   c = a+ b
+// } else {
+//   c = a *b;
+// }
+/***************************** */
+//c = a > b ? a + b : a * b;
+const printC = function()  {
+  let c, a = 10, b = 3;
+if( a > b) {
+  c = a+ b
+} else {
+  c = a *b;
 }
-function minMaxArray(array,minMaxCur) {
-   
-    for(let i = 0; i < array.length; i++){
-        minMaxElement(array[i], minMaxCur)
-    }
-  
+console.log(c)
 }
+printC()
 
-function minMaxElement(Element, minMaxCur) {
-    if (Element < minMaxCur[0]) {
-        minMaxCur[0] = Element;
-    } else if (Element > minMaxCur[1]){
-        minMaxCur[1] = Element;
-    }
-}
-console.log(`minMax(1, 2, 3)=${minMax(1, 2, 3)}`);
-console.log(`minMax(1, 2, 3, [100, 50] )=${minMax(1, 2, 3, [100, 50] )}`);
-console.log(`minMax(1, 2, 3, [100, 50], [-2, 40, 200])= ${minMax(1, 2, 3, [100, 50], [-2, 40, 200])}`);
-console.log(`minMax([-2, 40, 200], 1, 2, 3, [100, 50], -100, 300)=${minMax([-2, 40, 200], 1, 2, 3, [100, 50], -100, 300)}`)
-console.log(`minMax("abc", "12", "hello", ["-10", "kuku"], "kukureku")=${minMax("abc", "12", "hello", ["-10", "kuku"], "kukureku")}`)
+const printCarrow = (a, b) => console.log(a > b ? a + b : a * b);
+printCarrow(3, 10);
+console.log([10, -7, 100, 20].sort((a, b) => a - b))
+
+
