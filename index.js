aa7aac58c4537347654f5b9befaf3a097435d248
Yuragranovsky <yuragranovsky@gmail.com>
2025-03-09 17:01:35 +0200

    adds TODO comment for consistency with README

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const arr = [1, 2, 3];
 let first = 1;
 let second = 2;
 
-// write one code line (insead of three specified below) after which variable 'first" will contain 2 and second will contain 1
+// TODO write one code line (insead of three specified below) after which variable 'first" will contain 2 and second will contain 1
 [first, second] = [second, first]
 console.log(`second = ${second}`) //should be printed out 1
 console.log(`first = ${first}`) //should be printed out 2
