70f185b5df442d0388de1269f93d02ebfdb24905
Yuragranovsky <yuragranovsky@gmail.com>
2025-03-09 19:26:40 +0200

    Old prototype syntax

--- employee.js
+++ employee.js
@@ -1,12 +1,26 @@
-//arrow function cannot be a method of an object
+
 const empl1 = {name: "Vasya", basicSalary: 10000,
-    computeSalaryFun: function() {
+    computeSalary: function() {
         return this.basicSalary
-    },
-    computeSalaryArrow: () => {
-        return this.basicSalary;
     }
 }
-const salaryFun = empl1.computeSalaryFun();
-const salaryArrow = empl1.computeSalaryArrow();
+//factory method - method creating object is not used if an object has method/s
+function createEmployee(name, basicSalary) {
+    const res = {name, basicSalary};
+    res.computeSalary = function() {
+        return this.basicSalary
+    }
+}
+function Employee(name, basicSalary) {
+    this.name = name;
+    this.basicSalary = basicSalary;
+
+}
+Employee.prototype.computeSalary = function() {
+    return this.basicSalary
+}
+const empl2 = new Employee("Petya", 10000);
+const empl3 = new Employee("Sara", 12000);
+const empl2Salary = empl2.computeSalary();
+const empl3Salary = empl3.computeSalary();
 let a;
