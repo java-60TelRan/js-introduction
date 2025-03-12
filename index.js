adf8d35ef3790dce5d8051ff6968359f797885d9
Yuragranovsky <yuragranovsky@gmail.com>
2025-03-12 18:49:41 +0200

    prototype / this/ reduce for all arrays

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 import { testframework } from "./testframework.js";
+// Array.prototype.reduce = function() {
+//     return 0;
+// }
+// delete Array.prototype.reduce
+Array.prototype.sum = function() {
+    const res = this.reduce((acc,cur) => acc + cur);
+    return res;
+}
+testframework("Tesing new method for all arrays",
+    'const ar1 = [1, 2, 3]; const ar2 = [3, 4, "a"]',
+    ['ar1.sum();', 'ar2.sum();'],
+    [6, "7a"]
+)
 
-import WageEmployee from "./WageEmployee.js";
-import Manager from "./Manager.js";
-//writing tests for all Company methods
 
-testframework(
-  "Company methods testing",
-  "const company = new Company();" +
-    'company.addEmployee(new Employee("name1", 10000, "Dep1"));' +
-    'company.addEmployee(new WageEmployee("name2", 10000, "Dep1", 100, 100));' +
-    'company.addEmployee(new Manager("name3", 10000, "Dep2", 2));',
-    ['!!company.employees.name1 && !!company.employees.name2 && !!company.employees.name3',
-        'company.deleteEmployee(new Employee("name3")); !!company.employees.name3',
-        'company.getDepBudget("Dep1")',
-        'company.getEmployeesMaxSalary()',
-        'company.getEmployeesDepartment("Dep2")'
-    ],
-    [true, false, 30000, [new WageEmployee("name2", 10000, "Dep1", 100, 100),
-        new Manager("name3", 10000, "Dep2", 2)], [new Manager("name3", 10000, "Dep2", 2)]
-    ]
-);
--- testframework.js
+++ testframework.js
@@ -1,7 +1,4 @@
-import Employee from "./employee.js";
-import WageEmployee from "./WageEmployee.js";
-import Manager from "./Manager.js";
-import Company from "./Company.js";
+
 function test(commonScript, testObj) {
   //testObj structure {script: <string containg script text>, expected: <any type>}
   //returns resultObj with structure {script: <string containg script text>,
