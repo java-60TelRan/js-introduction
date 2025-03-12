23bca4fc91858dd42136890467f2682dbb71979c
Yuragranovsky <yuragranovsky@gmail.com>
2025-03-12 21:02:53 +0200

    HW #21 Definition (see ReadME)

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-# HW#20 Definition
-## Write all Company methods according to the comments (file Company.js)
-## Write tests for all Company methods
-### For testing methods addEmployee and deleteEmployee you may use direct access the field this.employees 
+# HW#21
+## Write class Deffered for printing out the following 
+const d = new Deferred()<br>
+d.then(function(res){ console.log("1 ", res); return "a"; });<br>
+d.then(function(res){ console.log("2 ", res); return "b"; });<br>
+d.then(function(res){ console.log("3 ", res); return "c"; });<br>
+d.resolve('hello');<br><br>
+
+1  hello<br>
+2  a<br>
+3  b<br>
+## Write method myBind  with the same behavior as the standard "bind" method
+### You may use any standard methods except "bind"
--- index.js
+++ index.js
@@ -1,34 +1,4 @@
 import { testframework } from "./testframework.js";
-class X {
-    constructor(arr) {
-        this.arr = arr;
-    }
-    logElements() {
-        this.arr.forEach(e => console.log(e))
-    }
-    sumElements() {
-        return this.arr.reduce((acc, cur) => acc + cur);
-    }
-    action(fun) {
-        return fun.call(this);
-    }
-    joinElements() {
-        return this.arr.join();
-    }
-    actionBind(fun) {
-        return fun();
-    }
-   
-
-}
-const objX = new X([1, 2, 3,20, 40]);
-// objX.logElements();
-// console.log(objX.sumElements());
-//objX.action(objX.logElements);
-// console.log(objX.action(objX.sumElements));
-// console.log(objX.action(objX.joinElements));
-console.log(objX.actionBind(objX.sumElements.bind(objX)))
-
 
 
 
