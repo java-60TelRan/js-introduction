fe5ef56f3f6b88369ba217c8584f61aa5fb14628
Yuragranovsky <yuragranovsky@gmail.com>
2025-03-16 20:12:39 +0200

    synchronous sleep method

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
-function getUserPassword(probCorrectPass) {
-  const passwords = ['correct', 'wrong'];
-  const index =  Math.random() < probCorrectPass ? 0 : 1
-  return passwords[index];
-}
-function login(password) {
-  if (password !== 'correct') {
-    throw "wrong credentials";
+function sleep(timeout) {
+  let running = true;
+  const timestamp = new Date().getTime();
+  while(running) {
+    let current = new Date().getTime();
+    if (current - timestamp >= timeout) {
+      running = false;
+    }
   }
 }
-function getUserData(username) {
-  const users = {'Vasya': {name: "Vasya", age: 30},
-  'Petya': {name: "Petya", age:40}
-}
-if(!users[username]) {
-  throw `username ${username} doesn't exist`
-}
-return users[username]
-}
-function funStackExample(username) {
-  const password = getUserPassword(0.8);
-  try {
-    login(password);
-    const userData = getUserData(username);
-    console.log(userData);
-  } catch (error) {
-    console.log(`error: ${error}`)
-  }
-
-}
-funStackExample('Vasya');
+sleep(5000);
+console.log("Hello world");
