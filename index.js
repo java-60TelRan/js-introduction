6dd85a21964d1d5ca99a3d52d6ef18f7fffff2db
Yuragranovsky <yuragranovsky@gmail.com>
2025-03-16 19:50:49 +0200

    functional stack with example error handling (synchronous)

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Hello World");
+function getUserPassword(probCorrectPass) {
+  const passwords = ['correct', 'wrong'];
+  const index =  Math.random() < probCorrectPass ? 0 : 1
+  return passwords[index];
+}
+function login(password) {
+  if (password !== 'correct') {
+    throw "wrong credentials";
+  }
+}
+function getUserData(username) {
+  const users = {'Vasya': {name: "Vasya", age: 30},
+  'Petya': {name: "Petya", age:40}
+}
+if(!users[username]) {
+  throw `username ${username} doesn't exist`
+}
+return users[username]
+}
+function funStackExample(username) {
+  const password = getUserPassword(0.8);
+  try {
+    login(password);
+    const userData = getUserData(username);
+    console.log(userData);
+  } catch (error) {
+    console.log(`error: ${error}`)
+  }
+
+}
+funStackExample('Vasya');
