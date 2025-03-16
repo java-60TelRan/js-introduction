9d92d8d49a01b62b5352d1a773781223bd47defb
Yuragranovsky <yuragranovsky@gmail.com>
2025-03-16 20:58:21 +0200

    asynchronous sleep

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function sleep(timeout) {
-  let running = true;
-  const timestamp = new Date().getTime();
-  while(running) {
-    let current = new Date().getTime();
-    if (current - timestamp >= timeout) {
-      running = false;
+  return new Promise(resolve => {
+    if(timeout < 0) {
+      throw("timeout cannot be negative")
     }
-  }
+    setTimeout(() => resolve(), timeout)
+  })
 }
-sleep(5000);
+const timeout = -5000;
+sleep(timeout).then(() => console.log(`result after ${timeout / 1000} seconds` ))
+.catch(e => {console.log(e)}).finally(()=> console.log("printing out in any case"));
 console.log("Hello world");
