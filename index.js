592e9846c7192ae64c61632405d177f4f3f14ef0
Yuragranovsky <yuragranovsky@gmail.com>
2025-03-19 20:51:11 +0200

    drawing images from thecatapi

--- index.js
+++ index.js
@@ -1,46 +1,25 @@
-function getPromise(timeout, value) {
-  return new Promise(resolve => {
-    setTimeout(() => resolve(value), timeout)
-  })
-}
-function getUserPassword(probCorrectPass) {
-  const passwords = ['correct', 'wrong'];
-  const index =  Math.random() < probCorrectPass ? 0 : 1;
-  //timeout - 1 sec
-  return getPromise(1000, passwords[index]);
-}
-function login(password) {
-  //returns promise in the state resolved only for passowrd 'correct' otherwise state rejected
-  //timeout 2 sec
-  if (password !== 'correct') {
-    throw "wrong credentials"
-  }
-  return getPromise(2000);
-}
-function getUserData(username) {
-  const users = {'Vasya': {name: "Vasya", age: 30},
-  'Petya': {name: "Petya", age:40}}
-  //returns promise in the state resolved if username exists with returning user data
-  //otherwise state rejected with apropriate message
-  if(!users[username]) {
-    throw `user ${username} not found`
-  }
-  //timeout 1 sec
-  return getPromise(1000, users[username])
+//console.log(fetch("https://api.thecatapi.com/v1/breeds"))
+// fetch("https://api.thecatapi.com/v1/breeds")
+// .then(resp => resp.json()).then(data => console.log(data))
+const ulElem = document.getElementById("ul_elem"); 
+drawImages(ulElem);
+async function drawImages(elem) {
+  const response = await fetch("https://api.thecatapi.com/v1/breeds");
+  const data = await response.json();
+  const images = getImages(data);
+  const items = getItems(images);
+  elem.innerHTML = items;
 
-  }
-async function funStackExample(username) {
- try {
-  const password = await getUserPassword(0.5);
-  await login(password);
-  const userData = await getUserData(username);
-  console.log(userData)
- } catch (error) {
-  console.log(error, username)
- }
 }
-//Print out the message "Data processed" only after finishing both functions"
-const promise1 = funStackExample('Vasya');
-const promise2 = funStackExample('Petya');
-console.log("waiting for the data coming from the server...");
-Promise.race([promise1, promise2]).then(() => console.log("data processed"))
+function getImages(data) {
+  const images = data.map(record => `https://cdn2.thecatapi.com/images/${record.reference_image_id}.jpg`)
+  return images;
+}
+function getItems(images) {
+  const items = images.map(getItem);
+  return items.join("");
+}
+function getItem(image) {
+  const item = `<li><img src=${image} width="300" height="300"></li>`;
+  return item;
+}
