e90c4c2c9bdeb2924eee41e94b11d8ddb3f26466
Yuragranovsky <yuragranovsky@gmail.com>
2025-03-23 20:26:53 +0200

    Global var redefinition unlike let with no redefinition and no global scope

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-// scope
-//let has block- scope
-//var has function scope
-//variable leak: you define variable in block implying freeing of variable outside of block
+let iLet = 10;
+var iVar = 20;
+var iVar = 30;
+
+console.log('iVar from global Window object', window.iVar);
+console.log('iLet from global Window object', window.iLet)
 
 function funVar() {
-  for(var i = 0; i < 3 ; i++) {
-    i++;
+  var x = 10;
+  for(var x = 0; x < 5; x++) {
+
   }
-  console.log(i);
+console.log('var',x)
 }
-funVar();
+funVar()
 function funLet() {
-  for(let i = 0; i < 3 ; i++) {
-    i++;
+  let x = 10;
+  for(let x = 0; x < 5; x++) {
+
   }
-  console.log(i);
+console.log('let',x)
 }
-funLet();
+funLet()
