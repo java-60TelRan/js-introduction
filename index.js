d847409aec1d9937b1d98a7a7f7d868a2a27ed8c
Yuragranovsky <yuragranovsky@gmail.com>
2025-03-23 20:42:57 +0200

    missing definition for var / 'use strict'

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-fun(10);
-function fun(num) {
-  console.log(num);
-}
-b = 10;
-var b;
-console.log('b', b); 
+'use strict'
 a = 10;
-let a;
-console.log('a', a); //reference error
+
+console.log('a', window.a)
