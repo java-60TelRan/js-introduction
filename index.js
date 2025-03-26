090fc9fc2f35df82824f93c1f97519423773e603
Yuragranovsky <yuragranovsky@gmail.com>
2025-03-26 20:42:36 +0200

    Promise.all throw exception (reject) if at least one promise will be rejected

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const myPromise = () => Promise.resolve('I have resolved!');
+const promise1 = Promise.resolve('First')
+const promise2 = Promise.resolve('Second')
+const promise3 = Promise.reject('Third')
+const promise4 = Promise.resolve('Fourth')
 
-function firstFunction() {
-  myPromise().then(res => console.log(res));
-  console.log('second');
+const runPromises = async () => {
+	const res1 = await Promise.all([promise1, promise2])
+	const res2  = await Promise.all([promise3, promise4])
+	return [res1, res2]
 }
 
-async function secondFunction() {
-  console.log(await myPromise());
-  console.log('second');
-}
+runPromises()
+	.then(res => console.log(res))
+	.catch(err => console.log(err))
 
-firstFunction();
-secondFunction();
